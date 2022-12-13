534d60c645608ba63be29655466e9ffbcf0d917e
Mauro Farina <maurofarina@protonmail.com>
2022-12-13 16:24:28 +0100

    create Main class

--- src/main/java/it/units/inginf/italiandraughts/Main.java
+++ src/main/java/it/units/inginf/italiandraughts/Main.java
@@ -1,7 +1,8 @@
-package org.example;
+package it.units.inginf.italiandraughts;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
