36e069265c928e0c5633397669189b4207c015a4
Mauro Farina <maurofarina@protonmail.com>
2022-12-13 18:04:35 +0100

    Test getCoordinateX and getCoordinateY methods of Square

--- src/test/java/it/units/inginf/italiandraughts/SquareTest.java
+++ src/test/java/it/units/inginf/italiandraughts/SquareTest.java
@@ -22,4 +22,39 @@ public class SquareTest {
         SquareColor squareColor = square.getSquareColor();
         assertEquals(squareColor, SquareColor.WHITE);
     }
+
+    @Test
+    void checkSquareCoordinateX(){
+        // Arrange
+        int x=0, y=0; // coordinates for square A1
+        // Act
+        Square square;
+        try {
+            square = new Square(x, y);
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+        // Assert
+        char coordinateX = square.getCoordinateX();
+        assertEquals(coordinateX, 'A');
+    }
+
+    @Test
+    void checkSquareCoordinateY(){
+        // Arrange
+        int x=2, y=5; // coordinates for square C6
+        // Act
+        Square square;
+        try {
+            square = new Square(x, y);
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+        // Assert
+        char coordinateY = square.getCoordinateY();
+        assertEquals(coordinateY, '6');
+    }
+
 }
