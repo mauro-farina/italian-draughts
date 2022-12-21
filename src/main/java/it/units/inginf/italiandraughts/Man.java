bb19030c849cc6ade98e6434b12d69b323cedecc
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2022-12-21 15:18:10 +0100

    Create Man.java

---
+++ src/main/java/it/units/inginf/italiandraughts/Man.java
@@ -0,0 +1,22 @@
+package it.units.inginf.italiandraughts;
+import java.lang.Exception;
+
+public class Man implements Piece{
+    protected PieceColor color; // color of the piece, either black or white
+    protected Square square; // current square on which the piece is positioned
+    //protected ManSymbol symbol;
+
+    public Man(PieceColor color, Square square) throws Exception {
+        if(color == null){
+            throw new Exception("Color cannot be null");
+        } else {
+            this.color = color;
+        }
+        if(square.getSquareColor().equals(SquareColor.WHITE)) {
+            throw new Exception("Pieces cannot be on white squares");
+        } else {
+            this.square = square;
+        }
+    }
+
+}
