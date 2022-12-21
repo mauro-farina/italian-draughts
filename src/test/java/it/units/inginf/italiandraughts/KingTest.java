e403b81e6b393b436fc47c879165c4a59ba417bc
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2022-12-21 15:30:16 +0100

    Create KingTest.java

---
+++ src/test/java/it/units/inginf/italiandraughts/KingTest.java
@@ -0,0 +1,83 @@
+package it.units.inginf.italiandraughts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KingTest {
+
+    @Test
+    void checkManColor() {
+        King king;
+        try{
+            Square square = new Square(2, 5);
+            king = new King(PieceColor.BLACK, square);
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+        PieceColor expectedColor = PieceColor.BLACK;
+        PieceColor actualColor = king.getColor();
+        assertEquals(expectedColor, actualColor);
+    }
+
+    @Test
+    void checkManSquare(){
+        King king;
+        Square square;
+        try{
+            square = new Square(2, 5);
+            king = new King(PieceColor.BLACK, square);
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+        Square actualSquare = king.getSquare();
+        assertEquals(square, actualSquare);
+    }
+
+    @Test
+    void checkIsMan() {
+        King king;
+        try{
+            Square square = new Square(2, 5);
+            king = new King(PieceColor.BLACK, square);
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+        assertFalse(king.isMan());
+    }
+
+    @Test
+    void checkIsKing() {
+        King king;
+        try{
+            Square square = new Square(2, 5);
+            king = new King(PieceColor.BLACK, square);
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+        assertTrue(king.isKing());
+    }
+
+    @Test
+    void checkSetSquare() {
+        King king;
+        Square square;
+        Square newSquare;
+        try{
+            square = new Square(2, 5);
+            king = new King(PieceColor.BLACK, square);
+            newSquare = new Square(4, 5);
+            king.setSquare(newSquare);
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+        Square actualSquare = king.getSquare();
+        assertEquals(newSquare, actualSquare);
+    }
+
+}
