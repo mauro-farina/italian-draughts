e38bf8129c73fa8cc187c736f0ff312501c00249
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2022-12-21 15:38:29 +0100

    Add Builder

--- src/main/java/it/units/inginf/italiandraughts/Board.java
+++ src/main/java/it/units/inginf/italiandraughts/Board.java
@@ -2,7 +2,49 @@ package it.units.inginf.italiandraughts;
 
 public class Board {
 
-    public Board(){
-        
+    private final Square[][] boardSquares;
+    
+    private int numberWhitePiece;
+
+    private int numberBlackPiece;
+
+    public Board() throws Exception {
+        boardSquares = new Square[8][8];
+        for(byte i=0; i<8; i++){
+            for(byte j=0; j<8; j++) {
+                boardSquares[i][j] = new Square(i, j);
+            }
+            System.out.println();
+        }
+        numberWhitePiece = 0;
+        numberBlackPiece = 0;
+        initBoard();
     }
+
+    private void initBoard() throws Exception {
+        // add White initial pieces (rows 1, 2, 3)
+        for(byte i=0; i<3; i++){
+            for(byte j=0; j<8; j+=2){
+                if(i % 2 == 0 && j == 0){
+                    j--;
+                    continue;
+                }
+                this.boardSquares[i][j].updateSquare(SquareContent.WHITE_MAN);
+                numberWhitePiece++;
+            }
+        }
+        // add Black initial pieces (rows 6, 7, 8)
+        for(byte i=5; i<8; i++){
+            for(byte j=0; j<8; j+=2){
+                if(i % 2 == 0 && j == 0){
+                    j--;
+                    continue;
+                }
+                this.boardSquares[i][j].updateSquare(SquareContent.BLACK_MAN);
+                numberBlackPiece++;
+            }
+            System.out.println();
+        }
+    }
+    
 }
