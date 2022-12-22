26347c5a2f64c10e8044404b81997f61d6106f66
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2022-12-22 16:42:01 +0100

    Add 2 test for check the number of black and white pieces

--- src/test/java/it/units/inginf/italiandraughts/BoardTest.java
+++ src/test/java/it/units/inginf/italiandraughts/BoardTest.java
@@ -6,6 +6,32 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BoardTest {
 
     @Test
+    void checkNumberWhitePieces() {
+        int numberOfWhitePieces;
+        try {
+            Board board = new Board();
+            numberOfWhitePieces = board.getNumberWhitePieces();
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+        assertEquals(numberOfWhitePieces, 12);
+    }
+
+    @Test
+    void checkNumberBlackPieces() {
+        int numberOfBlackPieces;
+        try {
+            Board board = new Board();
+            numberOfBlackPieces = board.getNumberBlackPieces();
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+        assertEquals(numberOfBlackPieces, 12);
+    }
+    
+    @Test
     void checkGetNumberOfPieces() {
         int numberOfPieces;
         try {
