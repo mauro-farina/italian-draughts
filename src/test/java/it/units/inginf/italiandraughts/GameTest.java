7231044b256c7af6d28bf7688c51c79d6bd81e68
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2022-12-22 18:06:12 +0100

    Create GameTest.java

---
+++ src/test/java/it/units/inginf/italiandraughts/GameTest.java
@@ -0,0 +1,63 @@
+package it.units.inginf.italiandraughts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameTest {
+
+    @Test
+    void checkTurn() {
+        Game game;
+        try {
+            game = new Game();
+            game.changeTurn();
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+        assertEquals(game.getTurn(), Turn.BLACK);
+    }
+
+    @Test
+    void checkTurnCounter() {
+        Game game;
+        try {
+            game = new Game();
+            game.incrementTurnCounter();
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+        assertEquals(game.getTurnCounter(), 2);
+    }
+
+    @Test
+    void victoryConditionIsTrue() {
+        Game game;
+        try {
+            game = new Game();
+            for(int i = 0; i < 12; i++) {
+                game.getBoard().decrementNumberWhitePieces();
+            }
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+        assertTrue(game.checkVictoryCondition());
+    }
+
+    @Test
+    void victoryConditionIsFalse() {
+        Game game;
+        try {
+            game = new Game();
+            game.getBoard().decrementNumberWhitePieces();
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+        assertFalse(game.checkVictoryCondition());
+    }
+
+}
