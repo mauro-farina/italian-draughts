5d44872edde0a366646c64560dfc75506e8520c8
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2022-12-26 16:55:55 +0100

    Update Builder and method changeTurn(), add methods getPlayer1() getPlayer2()

--- src/main/java/it/units/inginf/italiandraughts/Game.java
+++ src/main/java/it/units/inginf/italiandraughts/Game.java
@@ -5,26 +5,46 @@ public class Game {
 
     private final Board board;
 
-    private PlayerColor turn; //= WHITE if it's turn of the white player, otherwise = BLACK
+    private final Player player1; //This is the white player
+    
+    private final Player player2; //This is the black player
+    
+    private Player currentTurn;
 
     private int turnCounter;
 
     public Game() throws Exception {
-        board = new Board();
-        turnCounter = 1;
-        turn = PlayerColor.WHITE;
+        if((player1 == null) || (player2 == null)) {
+            throw new Exception("One or both players are not valid");
+        } else if((player1.getColor() != PlayerColor.WHITE) || (player2.getColor() != PlayerColor.BLACK)) {
+            throw new Exception("player1 uses the white pieces, while player2 uses the black pieces");
+        } else {
+            this.board = new Board();
+            this.turnCounter = 1;
+            this.player1 = player1;
+            this.player2 = player2;
+            this.currentTurn = this.player1;
+        }
     }
 
     public Board getBoard() {
         return this.board;
     }
 
-    public PlayerColor getTurn() {
-        return this.turn;
+    public Player getCurrentTurn() {
+        return this.currentTurn;
     }
 
     public int getTurnCounter() {
         return this.turnCounter;
+    }
+    
+     public Player getPlayer1() {
+        return this.player1;
+    }
+
+    public Player getPlayer2() {
+        return this.player2;
     }
 
     public void incrementTurnCounter() {
@@ -32,12 +52,12 @@ public class Game {
     }
 
     public void changeTurn() throws Exception {
-        if(this.turn == null) {
-            throw new Exception("Turn cannot be null");
-        } else if(this.turn == PlayerColor.WHITE) {
-            this.turn = PlayerColor.BLACK;
+        if(this.currentTurn == player1) {
+            this.currentTurn = player2;
+        } else if(this.currentTurn == player2){
+            this.currentTurn = player1;
         } else {
-            this.turn = PlayerColor.WHITE;
+            throw new Exception("currentTurn cannot be null");
         }
         incrementTurnCounter();
     }
