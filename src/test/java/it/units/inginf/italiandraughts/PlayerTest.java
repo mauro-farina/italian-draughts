33ba31a9a0a4dd5e142a6689523ad3bab7d7c267
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2022-12-26 16:01:59 +0100

    Create PlayerTest.java

---
+++ src/test/java/it/units/inginf/italiandraughts/PlayerTest.java
@@ -0,0 +1,32 @@
+package it.units.inginf.italiandraughts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class PlayerTest {
+
+    @Test
+    void checkNickname() {
+        Player player;
+        try {
+            player = new Player("Luca", PlayerColor.WHITE);
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+        assertEquals("Luca", player.getNickname());
+    }
+
+    @Test
+    void checkColor() {
+        Player player;
+        try {
+            player = new Player("Luca", PlayerColor.WHITE);
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+        assertEquals(PlayerColor.WHITE, player.getColor());
+    }
+
+}
