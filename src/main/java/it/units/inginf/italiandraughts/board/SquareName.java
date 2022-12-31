b9fe46c654b92db4bb2b580e08a9585ae22c97b0
Mauro Farina <maurofarina@protonmail.com>
2022-12-31 14:45:48 +0100

    add constructor SquareName(SquareCoordinates)

--- src/main/java/it/units/inginf/italiandraughts/board/SquareName.java
+++ src/main/java/it/units/inginf/italiandraughts/board/SquareName.java
@@ -15,6 +15,36 @@ public class SquareName {
         this.column = column;
     }
 
+    public SquareName(SquareCoordinates squareCoordinates) throws Exception {
+        if(squareCoordinates == null) {
+            throw new Exception("square coordinates cannot be null");
+        }
+        int x = squareCoordinates.getCoordinateX();
+        int y = squareCoordinates.getCoordinateY();
+        switch (x) {
+            case 0 -> this.column = 'A';
+            case 1 -> this.column = 'B';
+            case 2 -> this.column = 'C';
+            case 3 -> this.column = 'D';
+            case 4 -> this.column = 'E';
+            case 5 -> this.column = 'F';
+            case 6 -> this.column = 'G';
+            case 7 -> this.column = 'H';
+            default -> throw new Exception("Coordinates accepted value are integers from 0 to 7 included");
+        }
+        switch (y) {
+            case 0 -> this.row = '1';
+            case 1 -> this.row = '2';
+            case 2 -> this.row = '3';
+            case 3 -> this.row = '4';
+            case 4 -> this.row = '5';
+            case 5 -> this.row = '6';
+            case 6 -> this.row = '7';
+            case 7 -> this.row = '8';
+            default -> throw new Exception("Coordinates accepted value are integers from 0 to 7 included");
+        }
+    }
+
     public char getRow() {
         return row;
     }
