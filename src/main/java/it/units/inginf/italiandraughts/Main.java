bdaf50202651150823917f4a798e8c011d9c6455
Mauro Farina <maurofarina@protonmail.com>
2023-01-01 13:40:10 +0100

    temporary main to try Game.start()

--- src/main/java/it/units/inginf/italiandraughts/Main.java
+++ src/main/java/it/units/inginf/italiandraughts/Main.java
@@ -1,8 +1,19 @@
 package it.units.inginf.italiandraughts;
 
+import it.units.inginf.italiandraughts.game.Game;
+import it.units.inginf.italiandraughts.game.Player;
+import it.units.inginf.italiandraughts.game.PlayerColor;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        new Game(
+                new Player("player one", PlayerColor.WHITE),
+                new Player("player two", PlayerColor.BLACK)
+        ).start();
 
+        // doesn't check if the square actually has a piece (ex: A4 to B5, A4 could be empty)
+        // despite exception thrown on "A2 to A3", piece actually moves to A3
+        // turns are not alternating
     }
 }
