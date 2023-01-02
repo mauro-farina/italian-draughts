246188a572d8caeb44ebd5bf6e085b615af5c57b
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-01-02 12:14:48 +0100

    Update Main.java

--- src/main/java/it/units/inginf/italiandraughts/Main.java
+++ src/main/java/it/units/inginf/italiandraughts/Main.java
@@ -5,11 +5,31 @@ import it.units.inginf.italiandraughts.game.Player;
 import it.units.inginf.italiandraughts.game.PlayerColor;
 
 public class Main {
+    
+    private final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
         new Game(
-                new Player("player one", PlayerColor.WHITE),
-                new Player("player two", PlayerColor.BLACK)
+                new Player(readName(PlayerColor.WHITE), PlayerColor.WHITE),
+                new Player(readName(PlayerColor.BLACK), PlayerColor.BLACK)
         ).start();
+    }
+    
+    private static String readName(PlayerColor color) throws Exception{
+        while(true) {
+            if(color == PlayerColor.WHITE) {
+                System.out.println("Type player one's name");
+            } else if (color == PlayerColor.BLACK) {
+                System.out.println("Type player two's name");
+            } else {
+                throw new Exception("Invalid color player");
+            }
+            String name = scanner.nextLine();
+            if((!name.equals("")) && (name.charAt(0) != ' ')) {
+                return name;
+            } else {
+                System.out.println("Invalid name, please try again");
+            }
+        }
     }
 }
