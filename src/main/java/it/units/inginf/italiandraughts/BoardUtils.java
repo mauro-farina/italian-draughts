6b0f4faa4818d8586a2f1c4bbd3153ceb57f9e0b
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-01-04 16:15:06 +0100

    Create BoardUtils.java

---
+++ src/main/java/it/units/inginf/italiandraughts/BoardUtils.java
@@ -0,0 +1,68 @@
+package it.units.inginf.italiandraughts;
+
+import it.units.inginf.italiandraughts.board.*;
+
+public class BoardUtils {
+
+    public static void removePiece(Board board, Piece piece) throws Exception {
+        if(piece == null) {
+            throw new Exception("Invalid Piece");
+        }
+        if(piece.getColor() == PieceColor.WHITE) {
+            for(int i = 0; i < board.getWhitePieces().size(); i++) {
+                if (board.getWhitePieces().get(i) == piece) {
+                    board.getWhitePieces().remove(i);
+                    break;
+                } else if(i == board.getWhitePieces().size() - 1) {
+                    throw new Exception("Invalid Piece");
+                }
+            }
+        } else if(piece.getColor() == PieceColor.BLACK) {
+            for(int i = 0; i < board.getBlackPieces().size(); i++) {
+                if (board.getBlackPieces().get(i) == piece) {
+                    board.getBlackPieces().remove(i);
+                    break;
+                } else if(i == board.getWhitePieces().size() - 1) {
+                    throw new Exception("Invalid Piece");
+                }
+            }
+        } else {
+            throw new Exception("Invalid PieceColor");
+        }
+    }
+
+    public static void manBecomesKing(Board board, Piece piece) throws Exception {
+        if(board == null) {
+            throw new Exception("Invalid Board");
+        }
+        if(piece == null) {
+            throw new Exception("Invalid Piece");
+        }
+        if(piece.getColor() == PieceColor.WHITE) {
+            for(int i = 0; i < board.getWhitePieces().size(); i++) {
+                if (board.getWhitePieces().get(i) == piece) {
+                    board.getWhitePieces().add(new King(piece.getColor(), board.getWhitePieces().get(i).getSquare()));
+                    board.getWhitePieces().get(i).getSquare().setSquareContent(SquareContent.WHITE_KING);
+                    board.getWhitePieces().remove(i);
+                    break;
+                } else if(i == board.getWhitePieces().size() - 1) {
+                    throw new Exception("Invalid Piece");
+                }
+            }
+        } else if(piece.getColor() == PieceColor.BLACK) {
+            for(int i = 0; i < board.getBlackPieces().size(); i++) {
+                if (board.getBlackPieces().get(i) == piece) {
+                    board.getBlackPieces().add(new King(piece.getColor(), board.getWhitePieces().get(i).getSquare()));
+                    board.getBlackPieces().get(i).getSquare().setSquareContent(SquareContent.WHITE_KING);
+                    board.getBlackPieces().remove(i);
+                    break;
+                } else if(i == board.getBlackPieces().size() - 1) {
+                    throw new Exception("Invalid Piece");
+                }
+            }
+        } else {
+            throw new Exception("This color is invalid");
+        }
+    }
+
+}
