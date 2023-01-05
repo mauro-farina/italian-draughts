bc39499d9df7189f3844f7540b088eb4e462c5f8
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-01-05 18:05:07 +0100

    update exception handling

--- src/main/java/it/units/inginf/italiandraughts/board/SquareCoordinates.java
+++ src/main/java/it/units/inginf/italiandraughts/board/SquareCoordinates.java
@@ -1,26 +1,28 @@
 package it.units.inginf.italiandraughts.board;
 
 import it.units.inginf.italiandraughts.Utils;
+import it.units.inginf.italiandraughts.exception.CoordinatesException;
+import it.units.inginf.italiandraughts.exception.SquareNameException;
 
 public class SquareCoordinates {
 
     private final int row; // coordinate Y
     private final int column; // coordinate X
 
-    public SquareCoordinates(int column, int row) throws Exception {
+    public SquareCoordinates(int column, int row) throws CoordinatesException {
         if(row < 0 || row >= 8) {
-            throw new Exception("Row value must be in 0 to 7 range");
+            throw new CoordinatesException("SquareCoordinates.SquareCoordinates() -> row value must be in 0 to 7 range");
         }
         if(column < 0 || column >= 8) {
-            throw new Exception("Column value must be in 0 to 7 range");
+            throw new CoordinatesException("SquareCoordinates.SquareCoordinates() -> column value must be in 0 to 7 range");
         }
         this.row = row;
         this.column = column;
     }
 
-    public SquareCoordinates(SquareName squareName) throws Exception {
+    public SquareCoordinates(SquareName squareName) throws CoordinatesException, SquareNameException {
         if(squareName == null) {
-            throw new Exception("squareName cannot be null");
+            throw new SquareNameException("SquareCoordinates.SquareCoordinates() because SquareName cannot be null");
         }
         this.column = Utils.convertToCoordinates(squareName.toString())[0];
         this.row = Utils.convertToCoordinates(squareName.toString())[1];
