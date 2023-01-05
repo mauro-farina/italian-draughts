bece5a5ec25efb099ca08898e7791bf91a751b7e
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-01-05 18:29:33 +0100

    update exception handling

--- src/main/java/it/units/inginf/italiandraughts/board/Man.java
+++ src/main/java/it/units/inginf/italiandraughts/board/Man.java
@@ -1,24 +1,26 @@
 package it.units.inginf.italiandraughts.board;
-import java.lang.Exception;
+
+import it.units.inginf.italiandraughts.exception.PieceColorException;
+import it.units.inginf.italiandraughts.exception.SquareException;
 
 public class Man implements Piece{
-    
     protected PieceColor pieceColor; // color of the piece, either black or white
     protected Square square; // current square on which the piece is positioned
 
-    public Man(PieceColor pieceColor, Square square) throws Exception {
-        if(pieceColor == null){
-            throw new Exception("Color cannot be null");
+    public Man(PieceColor pieceColor, Square square) throws PieceColorException, SquareException {
+        if((pieceColor != PieceColor.WHITE) && (pieceColor != PieceColor.BLACK)) {
+            throw new PieceColorException("Man.Man() does not accept this PieceColor");
         } else {
             this.pieceColor = pieceColor;
         }
         if(!square.getSquareColor().equals(SquareColor.BLACK)) {
-            throw new Exception("Pieces cannot be on white squares");
+            throw new SquareException("Man.Man() does not accept this Square," +
+                    " because pieces cannot be on white squares");
         } else {
             this.square = square;
         }
     }
-
+    
     @Override
     public PieceColor getColor() {
         return this.pieceColor;
@@ -30,9 +32,10 @@ public class Man implements Piece{
     }
 
     @Override
-    public void setSquare(Square newSquare) throws Exception {
+    public void setSquare(Square newSquare) throws SquareException {
         if(!newSquare.getSquareColor().equals(SquareColor.BLACK)) {
-            throw new Exception("Pieces cannot be on white squares");
+            throw new SquareException("Man.setSquare() does not accept this Square," +
+                    " because pieces cannot be on white squares");
         } else {
             this.square = newSquare;
         }
