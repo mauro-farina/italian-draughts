424da0094df79788837be80777c8286c7c420362
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-01-12 18:23:07 +0100

    add checkManNotCaptureKing()

--- src/test/java/it/units/inginf/italiandraughts/SingleCaptureTest.java
+++ src/test/java/it/units/inginf/italiandraughts/SingleCaptureTest.java
@@ -2,6 +2,10 @@ package it.units.inginf.italiandraughts;
 
 import it.units.inginf.italiandraughts.game.SingleCapture;
 import it.units.inginf.italiandraughts.board.Board;
+import it.units.inginf.italiandraughts.board.Man;
+import it.units.inginf.italiandraughts.board.King;
+import it.units.inginf.italiandraughts.board.SquareContent;
+import it.units.inginf.italiandraughts.board.PieceColor;
 import it.units.inginf.italiandraughts.board.SquareCoordinates;
 import org.junit.jupiter.api.Test;
 
@@ -38,12 +42,30 @@ public class SingleCaptureTest {
 
     @Test
     void checkPieceOnToCoordinatesIsKing() {
-        SingleCapture singleCapture;
         try {
-            singleCapture = new SingleCapture(new Board(),
+            SingleCapture singleCapture = new SingleCapture(new Board(),
                     new SquareCoordinates(1, 2),
                     new SquareCoordinates(2, 3));
             assertFalse(singleCapture.pieceOnToCoordinatesIsKing());
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void checkManNotCaptureKing() {
+        try {
+            Board board = new Board();
+            board.getWhitePieces().clear();
+            board.getBlackPieces().clear();
+            board.getSquare(new SquareCoordinates(0, 3)).setSquareContent(SquareContent.WHITE_MAN);
+            Man whiteMan = new Man(PieceColor.WHITE, board.getSquare(new SquareCoordinates(0, 3)));
+            board.getSquare(new SquareCoordinates(1, 4)).setSquareContent(SquareContent.BLACK_KING);
+            King blackKing = new King(PieceColor.BLACK, board.getSquare(new SquareCoordinates(1, 4)));
+            assertFalse(new SingleCapture(board,
+                    new SquareCoordinates(0, 3),
+                    new SquareCoordinates(1, 4)).isValid());
         } catch (Exception e) {
             fail();
             throw new RuntimeException(e);
