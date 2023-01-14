40e405bad98ae433c928d8f323f51a1c7d0d2b6d
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-01-14 19:41:41 +0100

    add test checkTripleCapture()

--- src/test/java/it/units/inginf/italiandraughts/ObligatoryCaptureTest.java
+++ src/test/java/it/units/inginf/italiandraughts/ObligatoryCaptureTest.java
@@ -36,4 +36,35 @@ public class ObligatoryCaptureTest {
             throw new RuntimeException(e);
         }
     }
+    
+    @Test
+    void checkTripleCapture() {
+        try {
+            Board board = new Board();
+            board.getWhitePieces().clear();
+            board.getBlackPieces().clear();
+            for(int i = 0; i < 8; i++) {
+                for(int j = 0; j < 8; j++) {
+                    board.getSquare(new SquareCoordinates(j, i)).setSquareContent(SquareContent.EMPTY);
+                }
+            }
+            board.getSquare(new SquareCoordinates(3, 6)).setSquareContent(SquareContent.WHITE_MAN);
+            Man whiteMan1 = new Man(PieceColor.WHITE, board.getSquare(new SquareCoordinates(3, 6)));
+            board.getSquare(new SquareCoordinates(3, 4)).setSquareContent(SquareContent.WHITE_MAN);
+            Man whiteMan2 = new Man(PieceColor.WHITE, board.getSquare(new SquareCoordinates(3, 4)));
+            board.getSquare(new SquareCoordinates(1, 2)).setSquareContent(SquareContent.WHITE_MAN);
+            Man whiteMan3 = new Man(PieceColor.WHITE, board.getSquare(new SquareCoordinates(1, 2)));
+            board.getSquare(new SquareCoordinates(2, 7)).setSquareContent(SquareContent.BLACK_MAN);
+            Man blackMan = new Man(PieceColor.BLACK, board.getSquare(new SquareCoordinates(2, 7)));
+            board.getWhitePieces().add(whiteMan1);
+            board.getWhitePieces().add(whiteMan2);
+            board.getWhitePieces().add(whiteMan3);
+            board.getBlackPieces().add(blackMan);
+            assertEquals(ObligatoryCapture.getObligatoryCapture(board, whiteMan1).size(), 3);
+        } catch (Exception e) {
+            fail();
+            throw new RuntimeException(e);
+        }
+    }
+    
 }
