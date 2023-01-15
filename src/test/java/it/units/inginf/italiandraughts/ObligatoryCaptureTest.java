ad055ac03e7b8fa038559e533a8804b9dee11a31
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-01-15 13:46:43 +0100

    update all tests

--- src/test/java/it/units/inginf/italiandraughts/ObligatoryCaptureTest.java
+++ src/test/java/it/units/inginf/italiandraughts/ObligatoryCaptureTest.java
@@ -1,6 +1,9 @@
 package it.units.inginf.italiandraughts;
 
 import it.units.inginf.italiandraughts.game.ObligatoryCapture;
+import it.units.inginf.italiandraughts.game.Game;
+import it.units.inginf.italiandraughts.game.PlayerColor;
+import it.units.inginf.italiandraughts.game.Player;
 import it.units.inginf.italiandraughts.board.Board;
 import it.units.inginf.italiandraughts.board.Man;
 import it.units.inginf.italiandraughts.board.King;
@@ -16,7 +19,10 @@ public class ObligatoryCaptureTest {
     @Test
     void checkKingCaptureMan() {
         try {
-            Board board = new Board();
+            Game game = new Game(new Player("1", PlayerColor.WHITE),
+                    new Player("2", PlayerColor.BLACK));
+            game.initGame();
+            Board board = game.getBoard();
             board.getWhitePieces().clear();
             board.getBlackPieces().clear();
             for(int i = 0; i < 8; i++) {
@@ -25,22 +31,26 @@ public class ObligatoryCaptureTest {
                 }
             }
             board.getSquare(new SquareCoordinates(0, 5)).setSquareContent(SquareContent.WHITE_KING);
-            King whiteKing = new King(PieceColor.WHITE, board.getSquare(new SquareCoordinates(0, 5)));
             board.getSquare(new SquareCoordinates(1, 4)).setSquareContent(SquareContent.BLACK_MAN);
-            Man blackMan = new Man(PieceColor.BLACK, board.getSquare(new SquareCoordinates(1, 4)));
-            board.getWhitePieces().add(whiteKing);
-            board.getBlackPieces().add(blackMan);
-            assertEquals(ObligatoryCapture.getObligatoryCapture(board, blackMan).size(), 1);
+            board.getWhitePieces().add(new King(PieceColor.WHITE,
+                    board.getSquare(new SquareCoordinates(0, 5))));
+            board.getBlackPieces().add(new Man(PieceColor.BLACK,
+                    board.getSquare(new SquareCoordinates(1, 4))));
+            assertEquals(ObligatoryCapture.getObligatoryCaptureList(game).size(), 1);
         } catch (Exception e) {
             fail();
             throw new RuntimeException(e);
         }
     }
-    
+
     @Test
     void checkTripleCapture() {
         try {
-            Board board = new Board();
+            Game game = new Game(new Player("1", PlayerColor.WHITE),
+                    new Player("2", PlayerColor.BLACK));
+            game.initGame();
+            game.changeTurn();
+            Board board = game.getBoard();
             board.getWhitePieces().clear();
             board.getBlackPieces().clear();
             for(int i = 0; i < 8; i++) {
@@ -49,22 +59,21 @@ public class ObligatoryCaptureTest {
                 }
             }
             board.getSquare(new SquareCoordinates(3, 6)).setSquareContent(SquareContent.WHITE_MAN);
-            Man whiteMan1 = new Man(PieceColor.WHITE, board.getSquare(new SquareCoordinates(3, 6)));
             board.getSquare(new SquareCoordinates(3, 4)).setSquareContent(SquareContent.WHITE_MAN);
-            Man whiteMan2 = new Man(PieceColor.WHITE, board.getSquare(new SquareCoordinates(3, 4)));
             board.getSquare(new SquareCoordinates(1, 2)).setSquareContent(SquareContent.WHITE_MAN);
-            Man whiteMan3 = new Man(PieceColor.WHITE, board.getSquare(new SquareCoordinates(1, 2)));
             board.getSquare(new SquareCoordinates(2, 7)).setSquareContent(SquareContent.BLACK_MAN);
-            Man blackMan = new Man(PieceColor.BLACK, board.getSquare(new SquareCoordinates(2, 7)));
-            board.getWhitePieces().add(whiteMan1);
-            board.getWhitePieces().add(whiteMan2);
-            board.getWhitePieces().add(whiteMan3);
-            board.getBlackPieces().add(blackMan);
-            assertEquals(ObligatoryCapture.getObligatoryCapture(board, whiteMan1).size(), 3);
+            board.getWhitePieces().add(new Man(PieceColor.WHITE,
+                    board.getSquare(new SquareCoordinates(3, 6))));
+            board.getWhitePieces().add(new Man(PieceColor.WHITE,
+                    board.getSquare(new SquareCoordinates(3, 4))));
+            board.getWhitePieces().add(new Man(PieceColor.WHITE,
+                    board.getSquare(new SquareCoordinates(1, 2))));
+            board.getBlackPieces().add(new Man(PieceColor.BLACK,
+                    board.getSquare(new SquareCoordinates(2, 7))));
+            assertEquals(ObligatoryCapture.getObligatoryCaptureList(game).size(), 3);
         } catch (Exception e) {
             fail();
             throw new RuntimeException(e);
         }
     }
-    
 }
