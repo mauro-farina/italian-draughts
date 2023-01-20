a44f0febdebe6742fd504745be68bf2e117fe9e9
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-01-20 19:10:33 +0100

    Update Main.java

--- src/main/java/it/units/inginf/italiandraughts/Main.java
+++ src/main/java/it/units/inginf/italiandraughts/Main.java
@@ -16,10 +16,11 @@ public class Main {
 
     public static void main(String[] args) {
         try{
+            /*
             new Game(
                     new Player(readName(PlayerColor.WHITE), PlayerColor.WHITE),
                     new Player(readName(PlayerColor.BLACK), PlayerColor.BLACK)
-            ).start();
+            ).start();*/
         } catch(Exception e) {
             System.out.println(e.getMessage());
         }
