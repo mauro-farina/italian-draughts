f69f89393f7c6cdfc13db112ca4724ec8d8f7a4f
Mauro Farina <maurofarina@protonmail.com>
2023-01-21 12:14:25 +0100

    refactor

--- src/main/java/it/units/inginf/italiandraughts/BoardUtils.java
+++ src/main/java/it/units/inginf/italiandraughts/BoardUtils.java
@@ -13,6 +13,8 @@ import it.units.inginf.italiandraughts.exception.PieceColorException;
 import it.units.inginf.italiandraughts.exception.SquareException;
 import it.units.inginf.italiandraughts.exception.SquareContentException;
 
+import java.util.List;
+
 public class BoardUtils {
 
     public static void removePiece(Board board, Piece piece) throws BoardException, PieceException, PieceColorException {
@@ -45,32 +47,22 @@ public class BoardUtils {
         } else if (!piece.isMan()) {
             throw new PieceException("BoardUtils.toCrown() does not accept this Piece, because it is not a Man");
         }
-        if(piece.getColor() == PieceColor.WHITE) {
-            for(int i = 0; i < board.getWhitePieces().size(); i++) {
-                if (board.getWhitePieces().get(i) == piece) {
-                    board.getWhitePieces().add(new King(piece.getColor(), board.getWhitePieces().get(i).getSquare()));
-                    board.getWhitePieces().get(i).getSquare().setSquareContent(SquareContent.WHITE_KING);
-                    board.getWhitePieces().remove(i);
-                    break;
-                } else if(i == board.getWhitePieces().size() - 1) {
-                    throw new PieceException("BoardUtils.toCrown() does not accept this Piece," +
-                            " because it does not belong on the WhitePieces list");
-                }
+        if(piece.getColor() != PieceColor.BLACK && piece.getColor() != PieceColor.WHITE) {
+            throw new PieceColorException("BoardUtils.removePiece() does not accept this piece, because PieceColor is not valid");
+        }
+        List<Piece> piecesList = board.getPieces(piece.getColor());
+        for(int i = 0; i < piecesList.size(); i++) {
+            if (piecesList.get(i) == piece) {
+                piecesList.add(new King(piece.getColor(), piecesList.get(i).getSquare()));
+                piecesList.get(i).getSquare().setSquareContent(
+                        piece.getColor() == PieceColor.WHITE ? SquareContent.WHITE_KING : SquareContent.BLACK_KING
+                );
+                piecesList.remove(i);
+                break;
+            } else if(i == piecesList.size() - 1) {
+                throw new PieceException("BoardUtils.toCrown() does not accept this Piece," +
+                        " because it does not belong on the WhitePieces list");
             }
-        } else if(piece.getColor() == PieceColor.BLACK) {
-            for(int i = 0; i < board.getBlackPieces().size(); i++) {
-                if (board.getBlackPieces().get(i) == piece) {
-                    board.getBlackPieces().add(new King(piece.getColor(), board.getBlackPieces().get(i).getSquare()));
-                    board.getBlackPieces().get(i).getSquare().setSquareContent(SquareContent.BLACK_KING);
-                    board.getBlackPieces().remove(i);
-                    break;
-                } else if(i == board.getBlackPieces().size() - 1) {
-                    throw new PieceException("BoardUtils.toCrown() does not accept this Piece," +
-                            " because it does not belong on the BlackPieces list");
-                }
-            }
-        } else {
-            throw new PieceColorException("BoardUtils.toCrown() does not accept this PieceColor");
         }
     }
 
