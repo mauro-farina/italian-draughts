c6390852d98b0c2a8d905560386404f5ae040974
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-01-21 18:51:01 +0100

    delete commented code

--- src/test/java/it/units/inginf/italiandraughts/SquareTest.java
+++ src/test/java/it/units/inginf/italiandraughts/SquareTest.java
@@ -11,9 +11,7 @@ public class SquareTest {
 
     @Test
     void checkSquareColor() {
-        // Arrange
-        int x=0, y=0; // coordinates for square A1
-        // Act
+        int x=0, y=0;
         Square square;
         try {
             square = new Square(x, y);
@@ -21,16 +19,13 @@ public class SquareTest {
             fail();
             throw new RuntimeException(e);
         }
-        // Assert
         SquareColor squareColor = square.getSquareColor();
         assertEquals(squareColor, SquareColor.WHITE);
     }
 
     @Test
     void checkSquareCoordinateX(){
-        // Arrange
-        int x=0, y=0; // coordinates for square A1
-        // Act
+        int x=0, y=0;
         Square square;
         try {
             square = new Square(x, y);
@@ -44,9 +39,7 @@ public class SquareTest {
 
     @Test
     void checkSquareCoordinateY(){
-        // Arrange
-        int x=2, y=5; // coordinates for square C6
-        // Act
+        int x=2, y=5;
         Square square;
         try {
             square = new Square(x, y);
@@ -85,33 +78,5 @@ public class SquareTest {
         }
         assertFalse(square.isFree());
     }
-
-    /*
-    @Test
-    void checkWhenNotLastRow() {
-        int x=2, y=5;
-        Square square;
-        try{
-            square = new Square(x, y);
-        } catch(Exception e) {
-            fail();
-            throw new RuntimeException(e);
-        }
-        assertFalse(square.isLastRowFor(PieceColor.BLACK));
-    }
-
-    @Test
-    void checkWhenIsLastRow() {
-        int x=2, y=7;
-        Square square;
-        try{
-            square = new Square(x, y);
-        } catch(Exception e) {
-            fail();
-            throw new RuntimeException(e);
-        }
-        assertTrue(square.isLastRowFor(PieceColor.WHITE));
-    }
-    */
 
 }
