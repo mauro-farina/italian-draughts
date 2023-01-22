3723234111dec603cb43258be299a157706565f3
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-01-22 19:59:36 +0100

    correct warning

--- src/main/java/it/units/inginf/italiandraughts/io/CommandLineInputReader.java
+++ src/main/java/it/units/inginf/italiandraughts/io/CommandLineInputReader.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class CommandLineInputReader implements InputReader {
 
-    private Scanner scanner;
+    private final Scanner scanner;
 
     public CommandLineInputReader() {
         scanner = new Scanner(System.in);
