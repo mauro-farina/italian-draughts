1c4923c82295739414f3289222e5706f6f9714aa
Mauro Farina <maurofarina@protonmail.com>
2023-01-23 15:45:23 +0100

    extract isValid

--- src/main/java/it/units/inginf/italiandraughts/commands/CommandCapture.java
+++ src/main/java/it/units/inginf/italiandraughts/commands/CommandCapture.java
@@ -1,8 +1,13 @@
 package it.units.inginf.italiandraughts.commands;
 
-import it.units.inginf.italiandraughts.board.SquareCoordinates;
-import it.units.inginf.italiandraughts.board.SquareName;
+import it.units.inginf.italiandraughts.BoardUtils;
+import it.units.inginf.italiandraughts.board.*;
+import it.units.inginf.italiandraughts.exception.BoardException;
+import it.units.inginf.italiandraughts.exception.CommandException;
 import it.units.inginf.italiandraughts.exception.CoordinatesException;
+import it.units.inginf.italiandraughts.exception.SquareException;
+
+import java.util.List;
 
 public class CommandCapture extends Command {
 
@@ -30,6 +35,43 @@ public class CommandCapture extends Command {
         } catch (Exception exception) {
             throw new CoordinatesException("CommandCapture.CommandCapture() -> cannot capture piece on the edge of the board");
         }
+    }
+
+    public boolean isValid(Board board) throws SquareException, BoardException, CommandException {
+        if(board == null) {
+            throw new BoardException("Invalid command: board is null");
+        }
+        if(this.fromCoordinates == null || this.pieceToCaptureCoordinates == null || this.toCoordinates == null) {
+            return false;
+        }
+        Square selectedPieceSquare = board.getSquare(this.fromCoordinates);
+        Square capturedPieceSquare = board.getSquare(this.pieceToCaptureCoordinates);
+        Piece selectedPiece = BoardUtils.researchPiece(board, selectedPieceSquare);
+        Piece capturedPiece = BoardUtils.researchPiece(board, capturedPieceSquare);
+
+        if(selectedPiece == null) {
+            throw new CommandException("There are no pieces on " + selectedPieceSquare.getSquareName());
+        }
+        if(capturedPiece == null) {
+            throw new CommandException("There are no pieces on " + selectedPieceSquare.getSquareName());
+        }
+        if(selectedPiece.getColor() == capturedPiece.getColor()) {
+            throw new CommandException("You cannot capture your own pieces");
+        }
+        if(selectedPiece.isMan() && capturedPiece.isKing()) {
+            throw new CommandException("A man cannot capture a king");
+        }
+        if(!board.getSquare(toCoordinates).isFree()) {
+            throw new CommandException("Cannot capture piece on " + capturedPieceSquare.getSquareName()
+                    + ": there is a piece on " + board.getSquare(this.toCoordinates).getSquareName());
+        }
+        List<Square> reachableSquaresFromSelectedPiece = board.getReachableSquares(selectedPiece);
+        for(Square reachableSquare : reachableSquaresFromSelectedPiece) {
+            if (capturedPieceSquare == reachableSquare) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public SquareCoordinates getFromCoordinates() {
