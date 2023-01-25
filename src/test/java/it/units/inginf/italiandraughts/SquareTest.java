8ad943629430c5162d69fdfce063f669e1661c35
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-01-25 19:02:15 +0100

    update imports

--- src/test/java/it/units/inginf/italiandraughts/SquareTest.java
+++ src/test/java/it/units/inginf/italiandraughts/SquareTest.java
@@ -5,7 +5,9 @@ import it.units.inginf.italiandraughts.board.SquareColor;
 import it.units.inginf.italiandraughts.board.SquareContent;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class SquareTest {
 
