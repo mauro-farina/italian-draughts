a74a58f025ec242482f04d83937dab3052a87ebc
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-01-25 19:05:21 +0100

    update imports

--- src/test/java/it/units/inginf/italiandraughts/ManTest.java
+++ src/test/java/it/units/inginf/italiandraughts/ManTest.java
@@ -3,9 +3,14 @@ package it.units.inginf.italiandraughts;
 import it.units.inginf.italiandraughts.board.Man;
 import it.units.inginf.italiandraughts.board.PieceColor;
 import it.units.inginf.italiandraughts.board.Square;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class ManTest {
 
     @Test
