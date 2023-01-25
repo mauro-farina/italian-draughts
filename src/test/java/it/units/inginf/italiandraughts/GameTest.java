5ca7aec0f1f40ed4112d55ccc401830502ad56dc
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-01-25 19:06:45 +0100

    update imports

--- src/test/java/it/units/inginf/italiandraughts/GameTest.java
+++ src/test/java/it/units/inginf/italiandraughts/GameTest.java
@@ -9,9 +9,13 @@ import it.units.inginf.italiandraughts.game.Player;
 import it.units.inginf.italiandraughts.game.PlayerColor;
 import it.units.inginf.italiandraughts.io.CommandLineInputReader;
 import it.units.inginf.italiandraughts.io.CommandLineOutputPrinter;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class GameTest {
 
