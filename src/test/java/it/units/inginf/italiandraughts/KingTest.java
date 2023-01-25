22e7acb229d9681cced872c808b59a0af315404d
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-01-25 19:06:07 +0100

    update imports

--- src/test/java/it/units/inginf/italiandraughts/KingTest.java
+++ src/test/java/it/units/inginf/italiandraughts/KingTest.java
@@ -5,7 +5,10 @@ import it.units.inginf.italiandraughts.board.PieceColor;
 import it.units.inginf.italiandraughts.board.Square;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class KingTest {
 
