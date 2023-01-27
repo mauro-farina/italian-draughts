3f587e67b5835a7bd85fa3f34923c37fdd997a53
Mauro Farina <maurofarina@protonmail.com>
2023-01-27 13:21:17 +0100

    fix interface

--- src/main/java/it/units/inginf/italiandraughts/io/OutputPrinter.java
+++ src/main/java/it/units/inginf/italiandraughts/io/OutputPrinter.java
@@ -2,5 +2,7 @@ package it.units.inginf.italiandraughts.io;
 
 public interface OutputPrinter {
     void print(String stringToPrint);
-    void print(String stringToPrint, boolean error);
+    default void print(String stringToPrint, boolean error) {
+        // default body (empty) -> implementation is optional
+    }
 }
