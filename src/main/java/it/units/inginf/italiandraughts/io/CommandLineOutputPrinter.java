39fd7fd15d5927f19b2a9fa9e3a7590057b87979
Mauro Farina <maurofarina@protonmail.com>
2023-01-27 13:11:42 +0100

    add print-error method

--- src/main/java/it/units/inginf/italiandraughts/io/CommandLineOutputPrinter.java
+++ src/main/java/it/units/inginf/italiandraughts/io/CommandLineOutputPrinter.java
@@ -4,14 +4,25 @@ import java.io.PrintStream;
 
 public class CommandLineOutputPrinter implements OutputPrinter {
 
-    private final PrintStream printStream;
+    private final PrintStream printStreamForMessage;
+    private final PrintStream printStreamForError;
 
     public CommandLineOutputPrinter() {
-        this.printStream = System.out;
+        this.printStreamForMessage = System.out;
+        this.printStreamForError = System.err;
     }
 
     @Override
     public void print(String stringToPrint) {
-        printStream.println(stringToPrint);
+        printStreamForMessage.println(stringToPrint);
+    }
+
+    @Override
+    public void print(String stringToPrint, boolean error) {
+        if(error) {
+            printStreamForError.println(stringToPrint);
+        } else {
+            printStreamForMessage.println(stringToPrint);
+        }
     }
 }
--- src/main/java/it/units/inginf/italiandraughts/io/OutputPrinter.java
+++ src/main/java/it/units/inginf/italiandraughts/io/OutputPrinter.java
@@ -2,4 +2,5 @@ package it.units.inginf.italiandraughts.io;
 
 public interface OutputPrinter {
     void print(String stringToPrint);
+    void print(String stringToPrint, boolean error);
 }
