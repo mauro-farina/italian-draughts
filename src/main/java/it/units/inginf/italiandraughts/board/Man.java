5eece7f5f137d133bb48cb5a857a6269eeaf8bdb
Mauro Farina <maurofarina@protonmail.com>
2023-01-28 19:37:50 +0100

    refactor

--- src/main/java/it/units/inginf/italiandraughts/board/Man.java
+++ src/main/java/it/units/inginf/italiandraughts/board/Man.java
@@ -2,7 +2,6 @@ package it.units.inginf.italiandraughts.board;
 
 import it.units.inginf.italiandraughts.exception.PieceColorException;
 import it.units.inginf.italiandraughts.exception.SquareException;
-import it.units.inginf.italiandraughts.exception.PieceException;
 
 public class Man implements Piece{
     protected PieceColor pieceColor; // color of the piece, either black or white
@@ -10,31 +9,17 @@ public class Man implements Piece{
 
     public Man(PieceColor pieceColor, Square square) throws PieceColorException, SquareException {
         if((pieceColor != PieceColor.WHITE) && (pieceColor != PieceColor.BLACK)) {
-            if(isMan()) {
-                throw new PieceColorException("Man.Man() does not accept this PieceColor");
-            } else if (isKing()) {
-                throw new PieceColorException("King.King() does not accept this PieceColor");
-            }
-        } else {
-            this.pieceColor = pieceColor;
+            throw new PieceColorException((isMan() ? "Man" : "King") + "() does not accept this PieceColor");
         }
         if(square == null) {
-            if(isMan()) {
-                throw new SquareException("Man.man() does not accept this Square because it is null");
-            } else if(isKing()) {
-                throw new SquareException("King.king() does not accept this Square because it is null");
-            }
-        } else if(!square.getSquareColor().equals(SquareColor.BLACK)) {
-            if(isMan()) {
-                throw new SquareException("Man.Man() does not accept this Square," +
-                        " because pieces cannot be on white squares");
-            } else if(isKing()) {
-                throw new SquareException("King.King() does not accept this Square," +
-                        " because pieces cannot be on white squares");
-            }
-        } else {
-            this.square = square;
+            throw new SquareException((isMan() ? "Man" : "King") + "() does not accept this Square because it is null");
         }
+        if(!square.getSquareColor().equals(SquareColor.BLACK)) {
+            throw new SquareException((isMan() ? "Man" : "King") + "() does not accept this Square," +
+                        " because pieces cannot be on white squares");
+        }
+        this.pieceColor = pieceColor;
+        this.square = square;
     }
 
     @Override
@@ -48,24 +33,14 @@ public class Man implements Piece{
     }
 
     @Override
-    public void setSquare(Square newSquare) throws SquareException, PieceException {
-        if(square == null) {
-            if(isMan()) {
-                throw new SquareException("Man.setSquare() does not accept this Square because it is null");
-            } else {
-                throw new SquareException("King.setSquare() does not accept this Square because it is null");
-            }
-        } else if(!square.getSquareColor().equals(SquareColor.BLACK)) {
-            if(isMan()) {
-                throw new SquareException("Man.setSquare() does not accept this Square," +
-                        " because pieces cannot be on white squares");
-            } else {
-                throw new SquareException("King.setSquare()) does not accept this Square," +
-                        " because pieces cannot be on white squares");
-            }
-        } else {
-            this.square = newSquare;
+    public void setSquare(Square newSquare) throws SquareException {
+        if(square == null)
+            throw new SquareException((isMan() ? "Man" : "King") + ".setSquare() does not accept this Square because it is null");
+        if(!square.getSquareColor().equals(SquareColor.BLACK)) {
+            throw new SquareException((isMan() ? "Man" : "King") + ".setSquare() does not accept this Square," +
+                    " because pieces cannot be on white squares");
         }
+        this.square = newSquare;
     }
     @Override
     public boolean isMan() {
